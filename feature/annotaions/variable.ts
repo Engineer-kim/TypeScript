d02eef1d77e0dcf5b767b92c3fab681df69f8ef7
Kimhanjin <ddang0103@naver.com>
2025-01-12 21:56:59 +0900

    타입스크립트에서의 타입추론과 타입 any 정리

--- feature/annotaions/variable.ts
+++ feature/annotaions/variable.ts
@@ -24,3 +24,26 @@ const logNumber: (i: number) => void = (i: number) =>{
   console.log(i)
 }
 
+const json = '{"x": 10, "y": 20}';
+const coordinates: {x: number ; y: number} = JSON.parse(json); //JSON.parse 기본적으로 any 리턴함
+console.log(coordinates); // any 를 쓰면 타입스크립트 쓰는의미가 없음 => 명시적으로 타입 선언해주는게 좋음
+
+
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;  //불린 타입 혹은 숫자 타입 둘중에 하나를 가질수 있음
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
+
